0d41c15d37b0cb3a4c9049d0fc6a66b4eef78791
ParasJain1803 <paras2004jain@gmail.com>
2025-05-02 01:40:02 +0530

    feat: Implement dashboard with popular products and ratings

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import CardPopularProducts from "./CardPopularProducts";
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:overflow-auto gap-10 p-4 custom-grid-rows">
+      <CardPopularProducts />
+      <div className="bg-gray-400 row-span-3 xl:row-span-6" />
+      <div className="bg-gray-400 row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1" />
+      <div className="bg-gray-400 row-span-3" />
+      <div className="bg-gray-400 md:row-span-1 xl:row-span-2" />
+      <div className="bg-gray-400 md:row-span-1 xl:row-span-2" />
+      <div className="bg-gray-400 md:row-span-1 xl:row-span-2" />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
