924edca83618f5892b3ab9a05425c04c1f902e8f
ParasJain1803 <paras2004jain@gmail.com>
2025-05-02 23:23:01 +0530

    feat: Implement product management API with CRUD operations and integrate into server

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,7 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { get } from "http";
 
 export interface Product {
     productId: string;
+    name: string;
+    price: number;
+    rating?: number;
+    stockQuantity: number;
+  }
+
+  export interface NewProduct {
     name: string;
     price: number;
     rating?: number;
@@ -59,14 +67,28 @@ export interface User {
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_URL}),
     reducerPath : "api",
-    tagTypes: ["DashboardMetrics"],
+    tagTypes: ["DashboardMetrics", "Products"],
     endpoints: (build) => ({
         getDashboardMetrics: build.query<DashboardMetrics, void>({
             query: () => "/dashboard",
             providesTags: ["DashboardMetrics"],
-        })
-
+        }),
+        getProducts: build.query<Product[],string | void>({
+            query: (search) => ({
+              url: "/products",
+              params: search ? { search } : {},
+            }),
+            providesTags: ["Products"],
+        }),
+        createProduct: build.mutation<Product, NewProduct>({
+          query: (newProduct) => ({
+            url: "/products",
+            method: "POST",
+            body: newProduct,
+          }),
+          invalidatesTags: ["Products"],
+        }),
     }),
 })
 
-export const { useGetDashboardMetricsQuery } = api;
+export const { useGetDashboardMetricsQuery, useGetProductsQuery, useCreateProductMutation } = api;
